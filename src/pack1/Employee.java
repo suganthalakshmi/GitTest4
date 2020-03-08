cdc2b2651c6d6a323e75fef26799ba59cfa79978
suganthalakshmi <rsuganthi78@yahoo.com>
2020-03-08 15:24:47 +0530

    commit with employee.java addition

---
+++ src/pack1/Employee.java
@@ -0,0 +1,20 @@
+package pack1;
+
+public class Employee {
+
+	private Integer empId;
+	private String empName; 
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+}
